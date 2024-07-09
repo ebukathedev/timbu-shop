2f81e27a119a7a268fe330b40e15d93a4149f554
ebukathedev <ebukathedev@gmail.com>
2024-07-09 14:44:50 +0100

    refactor: Update product image imports in data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,64 +1,74 @@
-import product1 from "./assets/products/product_1.jpg";
-import product2 from "./assets/products/product_2.jpg";
-import product3 from "./assets/products/product_3.jpg";
-import product4 from "./assets/products/product_4.jpg";
-import product5 from "./assets/products/product_5.jpg";
-import product6 from "./assets/products/product_6.jpg";
+import product_1 from "./assets/products/product_1.jpg";
+import product_2 from "./assets/products/product_2.jpg";
+import product_3 from "./assets/products/product_3.jpg";
+import product_4 from "./assets/products/product_4.jpg";
+import product_5 from "./assets/products/product_5.jpg";
+import product_6 from "./assets/products/product_6.jpg";
+import product_7 from "./assets/products/product_7.jpg";
+import product_8 from "./assets/products/product_8.jpg";
+import product_9 from "./assets/products/product_9.jpg";
+import product_10 from "./assets/products/product_10.jpg";
 
 const products = [
 	{
 		id: 1,
-		product_image: product1,
+		product_image: product_1,
 		product_name: "Rosewater face Mist",
 		product_price: "5000.00",
 	},
 	{
 		id: 2,
-		product_image: product2,
+		product_image: product_2,
 		product_name: "Face Serum",
 		product_price: "5000.00",
 	},
 	{
 		id: 3,
-		product_image: product3,
+		product_image: product_3,
 		product_name: "Facial Skincare Set",
 		product_price: "15000.00",
 	},
 	{
 		id: 4,
-		product_image: product4,
+		product_image: product_4,
 		product_name: "Body Lotion",
 		product_price: "5000.00",
 	},
 	{
 		id: 5,
-		product_image: product6,
+		product_image: product_5,
 		product_name: "Moist Lip Balm",
 		product_price: "3000.00",
 	},
 	{
 		id: 6,
-		product_image: product5,
+		product_image: product_6,
 		product_name: "Restorative Hair Mask",
 		product_price: "5000.00",
 	},
 	{
 		id: 7,
-		product_image: product1,
-		product_name: "Rosewater face Mist",
+		product_image: product_7,
+		product_name: "Sunscreen",
 		product_price: "5000.00",
 	},
 	{
 		id: 8,
-		product_image: product2,
-		product_name: "Face Serum",
-		product_price: "5000.00",
+		product_image: product_8,
+		product_name: "Night time Skincare Set",
+		product_price: "10000.00",
 	},
 	{
 		id: 9,
-		product_image: product3,
-		product_name: "Facial Skincare Set",
-		product_price: "15000.00",
+		product_image: product_9,
+		product_name: "Daytime skincare set",
+		product_price: "10000.00",
+	},
+	{
+		id: 10,
+		product_image: product_10,
+		product_name: "Body Exfoliant",
+		product_price: "5000.00",
 	},
 ];
 
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -2,7 +2,7 @@ import { createPortal } from "react-dom";
 import Breadcrumb from "../components/Breadcrumb";
 import CartSuccess from "../components/modals/CartSuccess";
 import products from "../data";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartModalContext } from "../contexts/cart-modal-context";
 import ProductListing from "../components/ProductListing";
 import useScroll from "../contexts/hooks/use-scroll";
@@ -10,7 +10,9 @@ import useScroll from "../contexts/hooks/use-scroll";
 const modalElement = document.getElementById("modal");
 
 const Shop = () => {
+	const someProducts = products.slice(0, 6);
 	const { isCartSuccessModalOpen } = useContext(CartModalContext);
+	const [productDisplay, setProductDisplay] = useState(someProducts);
 
 	useScroll();
 
@@ -22,9 +24,12 @@ const Shop = () => {
 					isCartSuccessModalOpen &&
 					createPortal(<CartSuccess />, modalElement)}
 				<section className="mb-12 space-y-16 lg:space-y-24 lg:mb-28">
-					<ProductListing products={products} />
+					<ProductListing products={productDisplay} />
 					<div className="flex justify-center">
-						<button className="border text-primary-dark border-primary-dark rounded-[10px] w-fit py-[10px] px-4 font-bold xl:text-lg">
+						<button
+							onClick={() => setProductDisplay(products)}
+							className="border text-primary-dark border-primary-dark rounded-[10px] w-fit py-[10px] px-4 font-bold xl:text-lg"
+						>
 							View more
 						</button>
 					</div>
